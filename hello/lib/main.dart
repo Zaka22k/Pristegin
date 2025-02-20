648b7eee2e26913290ba6b61c4e449de050658b1
Zaka22k <pristegin941@mail.ru>
2025-02-20 12:04:34 +0300

    add: hello folder

---
+++ hello/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Инкремент'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 8,
+          children: <Widget>
+          [
+            Text(
+              'Значение инкремента:',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 8,
+                children: <Widget>
+                [
+                  FloatingActionButton(onPressed: _decrementCounter,child: const Icon(Icons.remove), backgroundColor: Colors.red,),
+                  FloatingActionButton(onPressed: _incrementCounter,child: const Icon(Icons.add), backgroundColor: Colors.green,),
+                ]
+            ),
+            TextButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.grey, // Цвет текста
+              ),
+              onPressed: _resetCounter,
+              child: Text("Сбросить"),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
